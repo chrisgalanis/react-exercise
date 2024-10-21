e8d461989647f1c679bae743c28b83801e8c5534
chrisgalanis <chrisgalan2004@gmail.com>
2024-10-21 13:21:30 +0300

    img => Nextjs Image

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface Product {
   imagePath: string;
@@ -8,25 +9,26 @@ interface Product {
 }
 
 const ProductCard = ({product}: {product: Product}) => {
-    
-    return (
-      <div className="product">
-        {
-          // Image at location : https://be.givelink.app/images/products/ + imagePath
-        }
-        <img
-          src={'https://be.givelink.app/images/products/' + product.imagePath}
-          alt={product.name}
-        ></img>
+  return (
+    <div className="product">
+      {
+        // Image at location : https://be.givelink.app/images/products/ + imagePath
+      }
+      <Image // Image Component (automatic resizes, format like webP)
+        src={'https://be.givelink.app/images/products/' + product.imagePath}
+        alt={product.name}
+        width={300}
+        height={300}
+      />
 
-        <div className="product-info">
-          <p>{product.name}</p>
-          <div className="price">
-            <p>{product.price} €</p>
-          </div>
+      <div className="product-info">
+        <p>{product.name}</p>
+        <div className="price">
+          <p>{product.price} €</p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ProductCard;
